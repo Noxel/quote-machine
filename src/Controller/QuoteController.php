2715ae81321a6bc38cba5236daf9147d09992a1f
Arescaldino <aresc002@pc02F.iut-rcc-info.urca>
2018-09-27 12:15:24 +0200

    recherche quote

--- src/Controller/QuoteController.php
+++ src/Controller/QuoteController.php
@@ -8,38 +8,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class QuoteController extends Controller{
 
+
+
     /**
      * @Route("/quotes")
      */
-    public function QuotesAction(){
-        $files = file_get_contents('../var/quotes.json');
-        $quotes = json_decode($files, true);
-        return $this->render('/quotes.html.twig',[
-            'quotes' => $quotes
-            ]);
-
-    }
-
-    /**
-     * @Route("/search")
-     */
     public function SearchAction(){
-        $quotes='';
+        $quotes=[];
         if(isset($_GET['search'])){
             $files = file_get_contents('../var/quotes.json');
             $json = json_decode($files, true);
             foreach ($json as $quote){
-                if(strpos($quote['quote'], $_GET['search']) !== false){
-                    
+                if(stripos($quote['quote'], $_GET['search']) !== false){
+                    $quotes[] = $quote;
                 }
 
             }
-
-
         }
-
-
-        return $this->render('/search.html.twig',[
+        return $this->render('/quotes.html.twig',[
             'quotes' => $quotes
         ]);
 
