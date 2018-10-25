4330ba92f17cf56c110c8f20436b694c89ff3fe7
Arescaldino <aresc002@pc02F.iut-rcc-info.urca>
2018-10-25 08:59:28 +0200

    Config tests fonctionnnels

---
+++ tests/QuoteControllerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class QuoteControllerTest extends WebTestCase
+{
+    public function testIndex(){
+
+
+    }
+}
